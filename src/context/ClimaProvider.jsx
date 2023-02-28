cf4f680e3f2f0e19ca1e79864df5f74cde436588
Marcos <marsanmar6291@gmail.com>
2023-02-28 16:07:57 -0600

    first commit

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -27,7 +27,7 @@ const ClimaProvider = ({children}) => {
         try {
             const { ciudad, pais} = datos;
 
-            const appId = 'f0762b1c93e353ff2797ff457396c057';
+            const appId = import.meta.env.VITE_API_KEY;
             
             const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
 
